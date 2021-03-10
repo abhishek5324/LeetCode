3eac94db6e59cfc4314f6119495e1c7b98c71b3c
Abhishek Bansal <abhishek2050.cse18@chitkara.edu.in>
2021-03-10 22:41:54 +0530

    FibonacciDP

--- FibonnaciDP.java
+++ FibonnaciDP.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
 class Solution {
     public int fib(int n) {
-        int storage[] = new int[n+1];
+        
+        
+        /*int storage[] = new int[n+1];
         Arrays.fill(storage,-1);
         return fib(n,storage);
     }
@@ -16,6 +18,24 @@ class Solution {
         
         int c = c1+c2;
         storage[n] = c;
-        return c;
+        return c;*/
+        if(n == 0 || n==1)
+            return n;
+        
+        int storage[] = new int[n+1];
+        storage[0] = 0;
+        storage[1] = 1;
+        
+        for(int i=2;i<=n;i++)
+        {
+            int c1 = storage[i-1];
+            int c2 = storage[i-2];
+            int ans = c1 + c2;
+            
+            storage[i] = ans;
+        }
+        return storage[n];
+        
+        
     }
 }
